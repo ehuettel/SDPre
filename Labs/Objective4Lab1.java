70f5942f112996996f82f5b580ae8722c78a2ff4
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-26 19:28:55 -0600

    complete Objective4Lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("What is your first name?");
+		String fname = "Eric";
+		System.out.println(fname);
+		System.out.println("What is your last name?");
+		String lname = "Huettel";
+		System.out.println(lname);
+		System.out.println("What is your favorite animal?");
+		String favoriteAnimal = "Cheetah";
+		System.out.println(favoriteAnimal);
+		System.out.println("What is your favorite food?");
+		String favoriteFood = "Chicken";
+		System.out.println(favoriteFood);
+		System.out.println("What is your favorite song?");
+		String favoriteSong = "The Morning";
+		System.out.println(favoriteSong);
+
+	}
+}
