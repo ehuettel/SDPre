8129fdaec559b93b56745779796ce1f8f2e437cb
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-26 18:44:17 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+	double num1 = 3;
+	double num2 = 5;
+	double num3 = 7;
+	System.out.println((num1 + num2 + num3) / 3);
+
+	}
+}
