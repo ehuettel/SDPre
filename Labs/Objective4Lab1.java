f567bec0de2ef3aea5e65cfb4f7799940c4205bd
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-26 22:50:24 -0600

    complete Objective4Lab1

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -3,24 +3,21 @@ import java.util.Scanner;
 public class Objective4Lab1 {
 	public static void main(String[] args) {
 
-		Scanner keyboard = new Scanner(System.in);
-        
-        System.out.println("What is your first name?");
-		String fname = "Eric";
-		System.out.println(fname);
-		System.out.println("What is your last name?");
-		String lname = "Huettel";
-		System.out.println(lname);
-		System.out.println("What is your favorite animal?");
-		String favoriteAnimal = "Cheetah";
-		System.out.println(favoriteAnimal);
-		System.out.println("What is your favorite food?");
-		String favoriteFood = "Chicken";
-		System.out.println(favoriteFood);
-		System.out.println("What is your favorite song?");
-		String favoriteSong = "The Morning";
-		System.out.println(favoriteSong);
-		System.out.println("My name is " + fname + " " + lname + ".");
+ 		Scanner keyboard = new Scanner(System.in);
+
+
+ 		System.out.println("What is your first name?");
+ 		String fname = keyboard.nextLine();
+ 		System.out.println("What is your last name?");
+ 		String lname = keyboard.nextLine();
+ 		System.out.println("What is your favorite animal?");
+ 		String favoriteAnimal = keyboard.nextLine();
+ 		System.out.println("What is your favorite food?");
+ 		String favoriteFood = keyboard.nextLine();
+ 		System.out.println("What is your favorite song?");
+ 		String favoriteSong = keyboard.nextLine();
+
+ 		System.out.println("My name is " + fname + " " + lname + ".");
 		System.out.println("My favorite animal is the " + favoriteAnimal + ".");
 		System.out.println("My favorite food is " + favoriteFood + ".");
 	 	System.out.println("My favorite song is " + favoriteSong + ".");
