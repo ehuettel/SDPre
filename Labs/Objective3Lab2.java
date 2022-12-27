2c1979bc2b5982edb8e298e6859f9950143a28fc
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-26 18:58:57 -0600

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = Math.sqrt(25);
+		double side2 = Math.sqrt(16);
+		double hypotenuse = side1 + side2;
+
+		System.out.println(hypotenuse);
+	}
+}
