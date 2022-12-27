908ce17ed288b9cd4def8de3749a30a22658e80c
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-26 23:07:57 -0600

    complete Objective4Lab2

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -4,29 +4,25 @@ public class Objective4Lab2 {
 	public static void main(String[] args) {
 	
 		Scanner keyboard = new Scanner(System.in);
-        int num1 = 3;
-        int num2 = 5;
-        int num3 = 7;
-        double dub1 = 3.3;
-        double dub2 = 5.5;
-        double dub3 = 7.7;
+     
+        System.out.println("Please enter the first whole number you would like to add.");
+        int num1 = keyboard.nextInt();
+        System.out.println("Please enter the second whole number you would like to add.");
+        int num2 = keyboard.nextInt();
+        System.out.println("Please enter the third whole number you would like to add");
+        int num3 = keyboard.nextInt();
+        System.out.println("Please enter the first deciaml you would like to add.");
+        double dub1 = keyboard.nextDouble();
+        System.out.println("Please enter the second decimal you would like to add.");
+        double dub2 = keyboard.nextDouble();
+        System.out.println("Please enter the third decimal you would like to add.");
+        double dub3 = keyboard.nextDouble();
+
         int numThree = num1 + num2 + num3;
         double dubThree = dub1 + dub2 + dub3;
-
-        System.out.println("Please enter the first whole number you would like to add.");
-        System.out.println(num1);
-        System.out.println("Please enter the second whole number you would like to add.");
-        System.out.println(num2);
-        System.out.println("Please enter the third whole number you would like to add.");
-        System.out.println(num3);
-        System.out.println("Please enter the first decimal number you would like to add.");
-        System.out.println(dub1);
-        System.out.println("Please enter the second deciaml number you would like to add");
-        System.out.println(dub2);
-        System.out.println("Please enter the third deciaml number you would like to add.");
-        System.out.println(dub3);
-        System.out.println("The sum of " + "num1" + " + " + "num2" + " + " + "num3" + " = " + numThree);
-        System.out.println("The sum of " + "dub1" + " + " + "dub2" + " + " + "dub3" + " = " + dubThree);
+       
+        System.out.println("The sum of " + num1 + " + " + num2 + " + " + num3 + " = " + numThree);
+        System.out.println("The sum of " + dub1 + " + " + dub2 + " + " + dub3 + " = " + dubThree);
 
 
 
