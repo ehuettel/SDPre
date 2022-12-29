7113319f69d273f8187ae7b47244235fff26d254
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-29 14:54:21 -0600

    complete Objective1Lab2

--- Labs/Objective1Lab2.java
+++ Labs/Objective1Lab2.java
@@ -4,7 +4,7 @@ public class Objective1Lab2 {
 		System.out.println ("| Grocery List |");
 		System.out.println ("| 1. Milk      |");
 		System.out.println ("| 2. Eggs      |");
-		System.out.println ("| 3. Bread     |");
+		System.out.println ("| 3. Bread     |"); 
 		System.out.println ("----------------");
 	}
 }
