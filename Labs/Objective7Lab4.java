8a6c1e4bd1f178334feabb5421e1ecf739b3faed
ehuettel <121079227+ehuettel@users.noreply.github.com>
2022-12-28 22:23:29 -0600

    complete Objective7Lab4

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -2,12 +2,14 @@ public class Objective7Lab4 {
 	public static void main(String[] args) {
 		int counter = 1;
 		int sum = counter;
-		while (counter <= 20) {
-			System.out.println(sum);
 
-			counter = counter + 1;
-			sum = counter + sum; 
+		while (counter < 20) {
+			
+			counter = counter + 1; 
+			sum += counter;
 
+			
 		}
+			System.out.println(sum);
 	}
 }
